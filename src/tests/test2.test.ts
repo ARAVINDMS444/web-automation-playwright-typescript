987c7af89d2ddc8319625cce1e6b6d2c17c5b998
Aravind MS <aravind.ms4444@gmail.com>
2025-07-04 22:31:28 +0530

    Commit 6

--- src/tests/test2.test.ts
+++ src/tests/test2.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test("get started link", async ({ page }) => {
   await page.goto("https://playwright.dev/");
@@ -6,7 +6,7 @@ test("get started link", async ({ page }) => {
   // Click the get started link
   await page.getByRole("link", { name: "Get started" }).click();
 
-  // Expects page to have a heading with the name of Installation.
+  // Expects page to have a heading with the name of Installation
   await expect(
     page.getByRole("heading", { name: "Installation" }),
   ).toBeVisible();
